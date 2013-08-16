71cf5ccd68acfcaf939b7aa990e61874084e19e8
PsyBurn <kresimir.antolic@infinum.hr>
2013-08-16 19:27:55 +0200

    added list item propagation. render list.

--- backbone.listview.js
+++ backbone.listview.js
@@ -6,32 +6,56 @@
     constructor: function ListView(options) {
       options = options || {};
       //if no collection is defined - setup a empty one
-      this.setupListView(options);
 
+      this.setupListView(options);
+      this.setupListeners();
       //super call - default Backbone magic
       Backbone.View.call(this, options);
     },
 
     setupListView: function(options) {
       _.defaults(options, {
-        itemView: Backbone.View
+        itemView: Backbone.View,
+        collection: new Backbone.Collection()
       });
+      this.listItems = [];
       this.itemView = options.itemView;
-      this.collection = this.collection || new Backbone.Collection();
-      this.collection.on('reset', this.addAll, this);
+      this.collection = options.collection;
+    },
+
+    setupListeners: function() {
+      this.listenTo(this.collection, 'add', this.addSingleItem, this);
+      this.listenTo(this.collection, 'reset', this.addAll, this);
+    },
+
+    render: function() {
+      this.addAll();
+      return this;
     },
 
     addAll: function() {
-      var fragment = document.createDocumentFragment();
-      this.collection.each(function(item, index, collection) {
-        fragment.appendChild(new this.itemView({
-          model: item
-        }).render().el);
+      this.collection.each(function(model) {
+        this.addSingleItem(model);
       }, this);
-      this.$el.html(fragment);
     },
 
+    addSingleItem: function(model) {
+      var viewItem = new this.itemView({
+        model: model
+      });
+      this.listItems.push(viewItem);
+      this.addListItemListeners(viewItem);
+      viewItem.render();
+      this.$el.append(viewItem.el);
+      return viewItem;
+    },
 
+    //propagate list item events through parent list view
+    addListItemListeners: function(view) {
+      this.listenTo(view, 'all', function(eventName) {
+        this.trigger('item:' + eventName);
+      }, this);
+    }
   });
 
   Backbone.ListView = List;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 (function() {
   'use strict';
   var TestView;
+  var testCollection = new Backbone.Collection([{id: 1, name: 'duck'}, {id: 2, name: 'dog'}]);
 
   module('Backbone.View', {
 
@@ -23,16 +24,78 @@
   });
 
   test('Render collection', function() {
-    var testCollection = new Backbone.Collection([{id: 1, name: 'duck'}, {id: 2, name: 'dog'}]);
     var ListView = Backbone.ListView.extend({
-      el: '#container',
-      className: 'list',
+      el: '<div></div>',
+      className: 'list'
+    });
+    var view = new ListView({
       collection: testCollection
     });
-    var view = new ListView();
-    testCollection.trigger('reset');
-    debugger
+    view.render();
     equal(view.$('div').length, 2, 'Should be 2 list items.');
+  });
+
+
+  test('Test event clicks on items', function() {
+    var clickCount = 0;
+    var SingleView = Backbone.View.extend({
+      className: 'list-item',
+      events: {
+        'click': 'clickMe'
+      },
+
+      clickMe: function() {
+        this.trigger('click');
+      }
+    });
+
+    var ListView = Backbone.ListView.extend({
+      el: '<div></div>',
+      className: 'list',
+    });
+
+    var view = new ListView({
+      collection: testCollection,
+      itemView: SingleView
+    });
+
+    view.render();
+    view.on('item:click', function() {
+      clickCount++;
+    });
+
+    view.listItems[0].trigger('click').trigger('click');
+
+    equal(clickCount, 2, 'Should be 2 clicks.');
+  });
+
+  test('Add list item', function() {
+    var SingleView = Backbone.View.extend({
+      className: 'list-item',
+      events: {
+        'click': 'clickMe'
+      },
+
+      clickMe: function() {
+        this.trigger('click');
+      }
+    });
+
+    var ListView = Backbone.ListView.extend({
+      el: '<div></div>',
+      className: 'list',
+    });
+
+    var view = new ListView({
+      collection: testCollection,
+      itemView: SingleView
+    });
+
+    view.render();
+
+    testCollection.add({id: 5, name: 'cat'});
+
+    equal(view.$('div').length, 3, 'Should be 3 list items.');
   });
 
 })();
